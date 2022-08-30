0be38646b418cec14eef192e2213076e57d27712
Bautista Rampinelli <jbrampinelli@gmail.com>
2022-08-29 23:12:41 -0300

    practica

--- index.js
+++ index.js
@@ -17,3 +17,69 @@ if (promedio >= 6) {
 }else{
     alert ("Está desaprobado con promedio: " + promedio)
 }
+//let nombre = prompt("Ingrese su nombre:")
+//let apellido = prompt("Ingrese su apellido:")
+//let edad = prompt("Ingrese su edad")
+//
+//
+//
+//const PERSONA = nombre+" "+apellido
+//console.log(PERSONA)
+//
+//if (PROMEDIO >= 6) {
+//    alert ("Estas aprobado")
+//} 
+//
+//if (USUARIO == "admin" && PASSWORD == "123456") {
+//    alert ("Bienvenido : " + USUARIO)
+//}else{
+//    alert ("Usuario o contraseña incorrectos")
+//}
+//for(let index = 1; index<=30; index++){
+//    const RESIDUO = index % 10
+//    if(RESIDUO == 0) {
+//        break
+//    }
+//
+//    console.log(index+" - "+ index% 10)
+//}
+//
+//let valor = prompt ("Ingrese un dato (ESC para salir)")
+//
+//while (valor.toUpperCase != "ESC") {
+//    alert("El usuario ingresó: " + valor)
+//    valor = prompt("Ingrese otro dato")
+//}
+//
+//let seleccionUsuario = parseInt(
+//    prompt(
+//        "Que desea pedir: \n 1. Hamburguesa \n 2. Tacos"
+//    )
+//)
+//
+//switch (seleccionUsuario){
+//    case 1:
+//        alert("Selecciono una hamburguesa")
+//    break
+//    case 2:
+//        alert("Usted selecciono Tacos")
+//    break
+//    default:
+//        alert("Opción desconocida")
+//    break
+//}
+//
+//function sumar (n1, n2, n3) {
+//    let suma = n1 + n2 + n3
+//    alert("La suma es " + suma)
+//}
+//let n1 = parseFloat(prompt("Ingrese el primer número"))
+//let n2 = parseFloat(prompt("Ingrese el segundo número"))
+//let n3 = parseFloat(prompt("Ingrese el tercer número"))
+//
+//sumar (n1, n2, n3)
+let valorUno = prompt ("ingrese el primer valor")
+let valorDos = prompt ("Ingrese el segundo valor")
+const suma = (valorUno, valorDos) => valorUno + valorDos
+
+alert("La suma es " + suma)
